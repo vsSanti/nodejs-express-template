9480cdfaf4186bca8dc3f4b28a0bce60aa4cf531
Vinicius Santiago <viniciussdsilva@gmail.com>
2022-03-14 17:12:15 -0300

    chore: remove initial test

--- src/index.ts
+++
@@ -1,4 +0,0 @@
-import { sum } from '@/calcs/sum';
-
-console.log("I'm alive!");
-console.log(`The sum of 1 and 2 is ${sum(1, 2)}`);
--- tests/calcs/sum.spec.ts
+++
@@ -1,7 +0,0 @@
-import { sum } from '@/calcs/sum';
-
-describe('Sum', () => {
-  it('should sum two numbers correctly', () => {
-    expect(sum(1, 2)).toBe(3);
-  });
-});
