678f8858e5e2ade3445664518ef8d54344e175e9
Vinicius Santiago <viniciussdsilva@gmail.com>
2022-03-14 19:22:19 -0300

    feat: express route adapter to catch thrown errors and return 500

--- src/main/adapters/express-route-adapter.ts
+++ src/main/adapters/express-route-adapter.ts
@@ -9,13 +9,17 @@ export const adaptRoute = (controller: Controller) => {
       params: req.params,
     };
 
-    const httpResponse = await controller.handle(request);
-    if (httpResponse.statusCode >= 200 && httpResponse.statusCode <= 299) {
-      res.status(httpResponse.statusCode).json(httpResponse.body);
-    } else {
-      res.status(httpResponse.statusCode).json({
-        error: httpResponse.body.message,
-      });
+    try {
+      const httpResponse = await controller.handle(request);
+      if (httpResponse.statusCode >= 200 && httpResponse.statusCode <= 299) {
+        res.status(httpResponse.statusCode).json(httpResponse.body);
+      } else {
+        res.status(httpResponse.statusCode).json({
+          error: httpResponse.body.message,
+        });
+      }
+    } catch (error) {
+      res.status(500).json({ message: 'Internal server error.' });
     }
   };
 };
