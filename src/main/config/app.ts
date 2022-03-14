70047ffe978daba4320ed19756e9fbfb467b57a9
Vinicius Santiago <viniciussdsilva@gmail.com>
2022-03-14 17:16:46 -0300

    feat: ensure Body Parser Middleware is setup

--- src/main/config/app.ts
+++ src/main/config/app.ts
@@ -1,6 +1,9 @@
 import express, { Express } from 'express';
 
+import setupMiddlewares from '@/main/config/middlewares';
+
 export const setupApp = async (): Promise<Express> => {
   const app = express();
+  setupMiddlewares(app);
   return app;
 };
